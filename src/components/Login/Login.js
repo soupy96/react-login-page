9a49df134aed085a431a7892220c93ccb00f357d
Michael Campbell <michael.campbell14@gmail.com>
2022-09-29 14:16:08 -0400

    adding comments back in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,6 +13,8 @@ import Input from '../UI/Input/Input';
 
 import classes from './Login.module.css';
 
+// this can be outside the component function because it doesnt need to interact with anything defined inside the component function
+// all the data used in this function will be passed into this function when its executed by React
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
     return { value: action.val, isValid: action.val.includes('@') };
@@ -55,28 +57,47 @@ const Login = (props) => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
-  useEffect(() => {
-    console.log('EFFECT RUNNING');
+  // useEffect(() => {
+  //   console.log('EFFECT RUNNING');
 
-    return () => {
-      console.log('EFFECT CLEANUP');
-    };
-  }, []);
+  //   return () => {
+  //     console.log('EFFECT CLEANUP');
+  //   };
+  // }, [enteredPassword]);
+  // useEffect without a second param runs AFTER every component render cycle
+  // with an empty array the useEffect will only run once and thats when the app starts up
+  // with a dependency it reruns whenever the component is re-evaludated and the state changes
+  // with a cleanup function it runs before the state function as a whole runs but not before the first time it runs
+  // with a cleaup function but no dependencies it'll run on the intial load and when the component is removed
+
+  // object destructuring
+  // we assign a alias to the extracted property
+  // isValid is in the emailState object which goes on the left of the colon(:)
+  // we then set w/e value is in isValid in the emailState object to the alias emailIsValid
+  // this can also be used with props
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
   useEffect(() => {
     const identifier = setTimeout(() => {
+      // this code only runs once for each keystroke
       console.log('Checking form Validity!');
       setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
 
     return () => {
+      // you can only return one thing, a function, clean-up function
+      // this runs before each useEffect execution except the very first one
       console.log('CLEANUP');
       clearTimeout(identifier);
     };
   }, [emailIsValid, passwordIsValid]);
+  // this useEffect will run when either of the two dependencies are changed. this could be state or props
+  // the useEffect will run code in response to an event on screen or something updating. this action is a response to another action
+  // we dont need to add state updating functions because React guarantees that they never change
+  // in browser api's like fetch() and local storage dont change cause they're not realted to the react component render cycle
+  // any component-internal variables wouldn't cause the component to be re-evaluated because its defined outside the component
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
